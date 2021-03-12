3d39a0ea5eb13a342ee2022eb85ce7e7c918a536
vernuskujinu <78141473+vernuskujinu@users.noreply.github.com>
2021-03-12 16:28:17 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+str = str.replace(/7/g, '|')
+
+    for (let i = 0, j = 0; i < str.length; i++) {
+        if ((str[i] == '8') && (j % 2 == 0)) {
+            str = str.replace('8', '(');
+            j++;
+        } else if ((str[i] == '8') && (j % 2 == 1)) {
+            str = str.replace('8', ')');
+            j++;
+        }
+    }
+
+    for (let i = 0, j = 0; i < str.length; i++) {
+        if ((str[i] == '|') && (j % 2 == 0)) {
+            str = str.replace('|', '<');
+            j++;
+        } else if ((str[i] == '|') && (j % 2 == 1)) {
+            str = str.replace('|', '>');
+            j++;
+        }
+    }
+
+    let o = bracketsConfig.reduce((acc, [open, close]) => ({ ...acc, [close]: open }), {});
+
+    if (o['|'] || o['7']) o['>'] = '<';
+    if (o['8']) o[')'] = '('
+
+
+    const stack = []
+    for (let s of str) {
+        if (!o[s]) {
+            stack.push(s)
+        } else if (stack.pop() !== o[s]) return false
+    }
+    return stack.length === 0
 }
